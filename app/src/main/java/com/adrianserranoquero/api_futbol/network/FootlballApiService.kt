7c5aff61ceb781e6495a9567f671fb6a13d28489
aserque2912 <xaserque692@ieshnosmachado.org>
2025-01-30 15:09:47 +0100

    commit recogida de datos api

---
+++ app/src/main/java/com/adrianserranoquero/api_futbol/network/FootlballApiService.kt
@@ -0,0 +1,19 @@
+package com.adrianserranoquero.api_futbol.network
+
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+data class Team(val id: Int, val name: String, val logo: String)
+
+data class TeamsResponse(val response: List<TeamWrapper>)
+data class TeamWrapper(val team: Team)
+
+interface FootballApiService {
+    @GET("teams")
+    suspend fun getTeams(
+        @Header("x-apisports-key") apiKey: String,
+        @Query("league") leagueId: Int,
+        @Query("season") season: Int
+    ): TeamsResponse
+}
